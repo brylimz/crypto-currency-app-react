b28777e75c018f5e8c0de93f18914fad55dcc753
brylimz <jackjaggeriv@gmail.com>
2022-04-21 08:30:41 +0700

    build navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import {Switch, Route, Link} from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd';
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="app">
+      <div className="navbar">
+      </div>
 
-export default App
+      <div className="main">
+        </div>
+
+      <div className="footer">
+        </div>
+
+    </div>
+  );
+};
+
+export default App;
